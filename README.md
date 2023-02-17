2cf36d76d3292064d4cac629e2b93833a2bd2da1
Mustafa <mustafa.asill@hotmail.com>
2023-02-17 17:42:29 +0300

    dbcontext

--- README.md
+++ README.md
@@ -3,6 +3,14 @@
 A sample N-layerd .NET Core Project with Clean Architecture and the generic repository pattern 
 
 ## Packages Installed
+
+### Infrastructure
+```
+Install-Package Microsoft.EntityFrameworkCore -v 6.0.14
+Install-Package Microsoft.EntityFrameworkCore.Tools -v 6.0.14
+Install-Package Npgsql.EntityFrameworkCore.PostgreSQL -v 6.0.8
+```
+
 ### ApplicationCore
 ```
 Install-Package Ardalis.Specification -v 6.1.0
---
+++ src/Infrastructure/Data/Config/ProductConfiguration.cs
@@ -0,0 +1,36 @@
+﻿using ApplicationCore.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Data.Config
+{
+    public class ProductConfiguration : IEntityTypeConfiguration<Product>
+    {
+        public void Configure(EntityTypeBuilder<Product> builder)
+        {
+            // ısrequired zorunlu değildi çünkü nulleb değil
+
+            builder.Property(x => x.Name).IsRequired().HasMaxLength(100);
+
+            builder.Property(x => x.Price).IsRequired().HasPrecision(18,2);
+
+            // nullabele oldugu için yazmasakta olur
+            builder.Property(x => x.PictureUri).IsRequired(false);
+
+
+            // bu iki satırada gerek yoktu çümkü product sınıfında ef geleneyine uygun yazarak ilişkilendirdik 
+
+            builder.HasOne(x => x.Category).WithMany().HasForeignKey(x => x.CategoryId);
+
+            builder.HasOne(x => x.Brand).WithMany().HasForeignKey(x => x.BrandId);
+
+
+        }
+    }
+}
